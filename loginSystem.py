a30d897ea05398dc7d931847aeb2c5fa6b08a1a2
ananyajain10 <jainananya355@gmail.com>
2023-09-10 00:37:44 +0530

    login system

---
+++ loginSystem.py
@@ -0,0 +1,21 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+print("YOUR ACCOUNT HAS BEEN CREATED SUCCESSFULLY!")
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+user1= User(username,password) 
+
+def Login():
+    Your_username = input("Enter your username: ")
+    Your_password = input("Enter your password: ")
+    if Your_username == user1.username and Your_password == user1.password:
+        print("Login successful!")
+    else:
+        print("Login failed!")
+
+print("LOGIN TO YOUR ACCOUNT")
+Login()
